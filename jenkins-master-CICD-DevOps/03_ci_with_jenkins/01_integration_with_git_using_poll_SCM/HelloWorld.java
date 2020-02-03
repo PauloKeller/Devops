f3f8f5d932b0f8c8ea2dacbad756082e4c3774c2
PauloKeller <paulo.vinicius.keller@gmail.com>
2020-02-03 12:42:28 -0300

    git web hook

--- jenkins-master-CICD-DevOps/03_ci_with_jenkins/01_integration_with_git_using_poll_SCM/HelloWorld.java
+++ jenkins-master-CICD-DevOps/03_ci_with_jenkins/01_integration_with_git_using_poll_SCM/HelloWorld.java
@@ -1,1 +1,1 @@
-public class HelloWorld { public static void main(String[] args) { System.out.println("------*****Hello world & welcome to jenkins Training*****------");}}
+public class HelloWorld { public static void main(String[] args) { System.out.println("------*****Hello world & welcome to jenkins Training, with web hook*****------");}}
