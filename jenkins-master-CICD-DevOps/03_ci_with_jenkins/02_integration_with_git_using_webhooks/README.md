c05bcf981bcfd778f31ad769d030995d0bc20ebc
PauloKeller <paulo.vinicius.keller@gmail.com>
2020-02-03 18:23:05 -0300

    maven project added

---
+++ jenkins-master-CICD-DevOps/03_ci_with_jenkins/02_integration_with_git_using_webhooks/README.md
@@ -0,0 +1,5 @@
+use ngrok to create a public ip 
+
+and expose jenkins localhost port
+
+use hooks only for small project
---
+++ jenkins-master-CICD-DevOps/03_ci_with_jenkins/03_jenkins_maven/src/main/java/com/paulokeller/App.java
@@ -0,0 +1,15 @@
+package com.paulokeller;
+
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+
+    public int addNumbers(int a, int b) {
+        return a+b;
+    }
+
+    public int subtractNumbers(int a, int b) {
+        return a-b;
+    }
+}
